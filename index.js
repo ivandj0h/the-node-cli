a08ab9ee0655aaf5f3d75e9e2c4c18241e68bf8f
ivandi1980 <ivandi.djoh@gmail.com>
2021-10-16 17:32:59 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+/**
+ * private-cli
+ * this is my first cli tool
+ *
+ * @author ivandi djoh gah <https://ivandjoh.online>
+ */
+
+const init = require('./utils/init');
+const cli = require('./utils/cli');
+const log = require('./utils/log');
+
+const input = cli.input;
+const flags = cli.flags;
+const { clear, debug } = flags;
+
+(async () => {
+	init({ clear });
+	input.includes(`help`) && cli.showHelp(0);
+
+	debug && log(flags);
+})();
